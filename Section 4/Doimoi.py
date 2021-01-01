255279de46765c27e431f55c06192f482f56e8a6
Phan Khac Cuong a.k.a jso <66241351+jiswoo@users.noreply.github.com>
2021-01-01 23:03:26 +0700

    Re-UP file .py

--- Section 4/Doimoi.py
+++ Section 4/Doimoi.py
@@ -1,1 +1,14 @@
-
+a, k, b, m, n= map(int,input().split())
+def binary_search():
+    first=0
+    last=int(10**18)
+    result = 0
+    while first <= last:
+        mid= (first + last)//2
+        if a * (mid-mid//k) + b * (mid-mid//m) >= n:
+            last=mid-1
+            result=mid
+        else:
+            first=mid+1
+    return result
+print(binary_search())
